a2da56a9fa94a6752b16be38ffcc16e1051b6ccd
MINSK\Aleh_Lipski <Aleh_Lipski@epam.com>
2017-09-25 18:25:20 +0300

    Add modules Importer and DirWatcher

---
+++ modules/DirWatcher.js
@@ -0,0 +1,9 @@
+export default class DirWatcher {
+    constructor() {
+        console.log('DirWatcher implementation');
+    }
+
+    watch(path, delay) {
+        console.log(delay);
+    }
+}
---
+++ modules/Importer.js
@@ -0,0 +1,13 @@
+export default class Importer {
+    constructor() {
+        console.log('Importer');
+    }
+
+    import(path) {
+        console.log(path);
+    }
+
+    importSync(path) {
+        console.log(path);
+    }
+}
